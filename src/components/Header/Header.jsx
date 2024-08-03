c7394021daca43d8387690dffc3fc45b8da26ac9
Federico Di Iorio <federico_metallica@hotmail.com>
2024-08-03 18:43:42 -0300

    Se incluyó el logo en el header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import classes from './Header.module.scss';
 import { FaBars } from "react-icons/fa6";
+import ditecnoImage from '../../assets/ditecnoNombre.png'
 
 const Header = () => {
     return (
         <header className={classes.background}>
             <div className={classes.container}>
-                <h1 className={classes.headerTitle}>Imagen de DiTecno</h1>
+                <img src={ditecnoImage} alt="Imagen de DiTecno" className={classes.headerTitle} />
                 <FaBars className={classes.headerIcon} />
             </div>
 
