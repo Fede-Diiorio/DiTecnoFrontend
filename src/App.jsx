87920713f69228ff7ef79c3b4f34101d8ea75c79
Federico Di Iorio <federico_metallica@hotmail.com>
2024-08-06 06:14:22 -0300

    Se creó el componente OpeningType

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer/Footer';
 import IndexBody from './components/IndexBody/IndexBody';
 import Product from './components/Product/Product'
 import ScrollToTop from './utils/ScrollToTop';
+import OpeningType from './components/OpeningType/OpeningType';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<IndexBody />} />
           <Route path='/product' element={<Product />} />
+          <Route path='/product/:pid/' element={<OpeningType />} />
         </Routes>
         <Footer />
       </BrowserRouter>
