9a6f97864fc53595860258650ffcee1790a08cd4
Federico Di Iorio <federico_metallica@hotmail.com>
2024-08-06 20:19:35 -0300

    Se agregó un botón de retorceso en Door y Window

--- src/components/Door/DoorOptions/DoorOptions.jsx
+++ src/components/Door/DoorOptions/DoorOptions.jsx
@@ -1,17 +1,23 @@
 import classes from './DoorOptions.module.scss';
 import Button from '../../Button/Button';
+import { TiArrowBack } from "react-icons/ti";
+import { Link } from 'react-router-dom';
 
 const DoorOptions = () => {
     return (
         <section className={classes.heroImage}>
-            <div className={classes.container}>
-                <div className={classes.doorOptions}>
-                    <h2>Tipo de apertura</h2>
-                    <p>Seleccione la dirección de apertura que desea para su puerta.</p>
-                    <div className={classes.options}>
-                        <Button to={'/door/inward'}>Exterior</Button>
-                        <Button to={'/door/outward'}>Interior</Button>
+            <div className={classes.shadow}>
+                <div className={classes.container}>
+                    <div className={classes.doorOptions}>
+                        <h2>Tipo de apertura</h2>
+                        <p>Seleccione la dirección de apertura que desea para su puerta.</p>
+                        <div className={classes.options}>
+                            <Button to={'/door/outward'}>Exterior</Button>
+                            <Button to={'/door/inward'}>Interior</Button>
+                        </div>
                     </div>
+                    <Link to={'/product'}><TiArrowBack className={classes.backArrow} /></Link>
+
                 </div>
             </div>
         </section>
--- src/components/Window/WindowOptions/WindowOptions.jsx
+++ src/components/Window/WindowOptions/WindowOptions.jsx
@@ -1,18 +1,23 @@
 import classes from './WindowOptions.module.scss';
 import Button from '../../Button/Button';
+import { TiArrowBack } from "react-icons/ti";
+import { Link } from 'react-router-dom';
 
 const WindowOptions = () => {
     return (
         <section className={classes.heroImage}>
-            <div className={classes.container}>
-                <div className={classes.windowOptions}>
-                    <h2>Tipo de apertura</h2>
-                    <p>Seleccione la dirección de apertura que desea para su puerta.</p>
-                    <div className={classes.options}>
-                        <Button to={'/window/inward'}>Exterior</Button>
-                        <Button to={'/window/outward'}>Interior</Button>
-                        <Button to={'/window/sliding'}>Corrediza</Button>
+            <div className={classes.shadow}>
+                <div className={classes.container}>
+                    <div className={classes.windowOptions}>
+                        <h2>Tipo de apertura</h2>
+                        <p>Seleccione la dirección de apertura que desea para su puerta.</p>
+                        <div className={classes.options}>
+                            <Button to={'/window/outward'}>Exterior</Button>
+                            <Button to={'/window/inward'}>Interior</Button>
+                            <Button to={'/window/sliding'}>Corrediza</Button>
+                        </div>
                     </div>
+                    <Link to={'/product'}><TiArrowBack className={classes.backArrow} /></Link>
                 </div>
             </div>
         </section>
