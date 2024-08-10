b1bf79912daedbdca0d572497c6b4ed00b197a67
Federico Di Iorio <federico_metallica@hotmail.com>
2024-08-10 16:17:41 -0300

    Se conectó con la api de forma exitosa0

--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -1,9 +1,18 @@
 import classes from './Selector.module.scss';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
-const Hero = ({ title, description, items }) => {
+const Hero = ({ title, description }) => {
+    const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+        fetch('http://localhost:8080/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch(error => console.error('Error en el fetch: ', error));
+    }, []);
+
+    console.log(products);
 
     return (
         <section className={classes.heroImage}>
@@ -11,9 +20,13 @@ const Hero = ({ title, description, items }) => {
                 <div className={classes.hero}>
                     <h2>{title}</h2>
                     <p>{description}</p>
-
-
-
+                    <ul>
+                        {products.map(product => (
+                            <li key={product.id}>
+                                {product.name}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </section>
