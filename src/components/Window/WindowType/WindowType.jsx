a16b3c5aae3466328d11410372843dfd8d050cdf
Federico Di Iorio <federico_metallica@hotmail.com>
2024-08-24 20:23:03 -0300

    Se mejoró el componente Selecto para contener mejor el contenido

--- src/components/Window/WindowType/WindowType.jsx
+++ src/components/Window/WindowType/WindowType.jsx
@@ -3,6 +3,7 @@ import { getWindowsTypes } from "../../../utils/getWindows";
 import { useEffect, useState } from "react";
 import Button from "../../Button/Button";
 import { useParams } from "react-router-dom";
+import classes from './WindowType.module.scss';
 
 const WindowType = () => {
     const { opening, style } = useParams();
@@ -16,10 +17,11 @@ const WindowType = () => {
 
     return (
         <>
-            <Selector title={'Tipo de abertuna'} description={'Seleccione el tipo de abertura que sea de su preferencia.'}>
+            <Selector title={'Tipo de abertuna'} description={'Seleccione la configuración de abertura que sea de su preferencia.'}>
                 <ul className='optionButtonflex'>
                     {colors.map(product => (
-                        <li key={product.id}>
+                        <li key={product.id} className={classes.li}>
+                            <img src={product.image} alt={`Imagen de ventana con ${product.name}`} />
                             <Button to={product.slug}>{product.name}</Button>
                         </li>
                     ))}
