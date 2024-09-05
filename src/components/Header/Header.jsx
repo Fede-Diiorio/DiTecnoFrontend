b3911447c5830f3b1c4402fe8666468e2e09d6da
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-05 17:48:17 -0300

    Se agregó funcionalidad al menú de hamburguesa

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,39 @@
+import React, { useState } from 'react';
 import classes from './Header.module.scss';
 import { FaBars } from "react-icons/fa6";
-import ditecnoImage from '../../assets/ditecnoNombre.png'
+import ditecnoImage from '../../assets/ditecnoNombre.png';
 import { Link } from 'react-router-dom';
 import CartWidget from '../CartWidget/CartWidget';
 
-
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(prevState => !prevState);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <header className={classes.background}>
             <div className={`container ${classes.container}`}>
                 <Link to={'/'}><img src={ditecnoImage} alt="Imagen de DiTecno" className={classes.headerTitle} /></Link>
                 <div className={classes.icons}>
                     <CartWidget />
-                    <FaBars className={classes.icon} />
+                    <FaBars className={classes.icon} onClick={toggleMenu} />
                 </div>
+            </div>
+
+            <div className={`container ${isMenuOpen ? classes.open : ''}`}>
+                <nav className={`${classes.dropdownMenu} ${isMenuOpen ? classes.open : ''}`}>
+                    <ul>
+                        <li><Link onClick={closeMenu} to="/">Inicio</Link></li>
+                        <li><Link onClick={closeMenu} to="/nosotros">Nosotros</Link></li>
+                        <li><Link onClick={closeMenu} to="/galeria">Galería</Link></li>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
