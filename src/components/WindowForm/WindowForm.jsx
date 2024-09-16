3f4ed0a5d01ce504fd89d374f48584863c23c1d5
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-16 18:33:30 -0300

    Se aisló la lógica del componente WindowForm

--- src/components/WindowForm/WindowForm.jsx
+++ src/components/WindowForm/WindowForm.jsx
@@ -8,111 +8,41 @@ import TwoCasement from './FieldsType/TwoCasement';
 import OneCasement from './FieldsType/OneCasement';
 import ThreeCasement from './FieldsType/ThreeCasement';
 import { useCart } from '../../context/CartContext';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { handleInputChange, initializeFormData, handleSubmit } from '../../utils/useWindowForm';
 
 const WindowForm = () => {
-
-    const { addItem } = useCart();
-    const navigate = useNavigate();
-    const successNotification = () => toast.success('¡Producto cargado!');
-    const alertNotification = () => toast.warn('Por favor, selecciona al menos un color.', {
-        autoClose: 5000,
-    });
-
     const { opening, style, type } = useParams();
     const [colors, setColors] = useState([]);
     const [typeSpecification, setTypeSpecification] = useState({});
-    const [formData, setFormData] = useState({
-        product: 'ventana',
-        opening,
-        style,
-        type,
-        width: '',
-        height: '',
-        casement: '',
-        casement2: '',
-        quantity: '',
-        glassType: '',
-        colors: [],
-    });
+    const [formData, setFormData] = useState(initializeFormData(opening, style, type));
 
+    const { addItem } = useCart();
+
+    // Obtener especificaciones de la ventana
     useEffect(() => {
-        getWindowSpecification(opening, style, type).then(data => {
-            setTypeSpecification(data);
-        });
+        getWindowSpecification(opening, style, type).then(setTypeSpecification);
     }, [opening, style, type]);
 
+    // Obtener colores disponibles
     useEffect(() => {
-        getColors().then(data => {
-            setColors(data);
-        });
+        getColors().then(setColors);
     }, []);
 
-    const handleInputChange = (e) => {
-        const { name, value, type, checked } = e.target;
-
-        if (type === "checkbox") {
-            setFormData(prevData => {
-                const updatedColors = checked
-                    ? [...prevData.colors, value]
-                    : prevData.colors.filter(color => color !== value);
-                return {
-                    ...prevData,
-                    colors: updatedColors
-                };
-            });
-        } else {
-            setFormData(prevData => {
-                let newValue;
-
-                if (name === 'quantity') {
-                    // Convertir a entero
-                    newValue = parseInt(value, 10) || '';
-                } else if (['width', 'height', 'casement', 'casement2'].includes(name)) {
-                    // Convertir a float
-                    newValue = parseFloat(value) || '';
-                } else {
-                    newValue = value;
-                }
-
-                return {
-                    ...prevData,
-                    [name]: newValue
-                };
-            });
-        }
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (formData.colors.length === 0) {
-            alertNotification();
-            return;
-        };
-
-        addItem(formData);
-        successNotification();
-        navigate('/');
-    };
-
+    // Renderizar componente según cantidad de hojas
     const renderCasementComponent = () => {
-        if (typeSpecification) {
-            if (typeSpecification.casementQuantity === 1) {
-                return <OneCasement formData={formData} handleInputChange={handleInputChange} />;
-            } else if (typeSpecification.casementQuantity === 2) {
-                return <TwoCasement formData={formData} handleInputChange={handleInputChange} />;
-            } else if (typeSpecification.casementQuantity === 3) {
-                return <ThreeCasement formData={formData} handleInputChange={handleInputChange} />;
-            }
-        }
+        if (typeSpecification.casementQuantity === 1) {
+            return <OneCasement formData={formData} handleInputChange={(e) => handleInputChange(e, formData, setFormData)} />;
+        } else if (typeSpecification.casementQuantity === 2) {
+            return <TwoCasement formData={formData} handleInputChange={(e) => handleInputChange(e, formData, setFormData)} />;
+        } else if (typeSpecification.casementQuantity === 3) {
+            return <ThreeCasement formData={formData} handleInputChange={(e) => handleInputChange(e, formData, setFormData)} />;
+        };
         return null;
     };
 
     return (
         <Selector title={'Cargar producto'} description={'Complete el formulario para cargar el producto al pedido'}>
-            <form className={classes.form} onSubmit={handleSubmit}>
+            <form className={classes.form} onSubmit={(e) => handleSubmit(e, formData, addItem, navigate, successNotification, alertNotification)}>
                 <div className={classes.formContainer}>
                     <img src={typeSpecification.image} alt={typeSpecification.name} className={classes.image} />
                     {renderCasementComponent()}
@@ -121,12 +51,12 @@ const WindowForm = () => {
                 <div className={classes.radios}>
                     <div className={classes.radio}>
                         <label htmlFor="dvhEstandar">Vidrio DVH estándar</label>
-                        <input type="radio" id="dvhEstandar" name="glassType" value="Estandar" onChange={handleInputChange} required />
+                        <input type="radio" id="dvhEstandar" name="glassType" value="Estandar" onChange={(e) => handleInputChange(e, formData, setFormData)} required />
                     </div>
 
                     <div className={classes.radio}>
                         <label htmlFor="dvhLaminado">Vidrio DVH laminado</label>
-                        <input type="radio" id="dvhLaminado" name="glassType" value="Laminado" onChange={handleInputChange} />
+                        <input type="radio" id="dvhLaminado" name="glassType" value="Laminado" onChange={(e) => handleInputChange(e, formData, setFormData)} />
                     </div>
                 </div>
 
@@ -139,7 +69,7 @@ const WindowForm = () => {
                                 name="color"
                                 value={color.slug}
                                 className={classes.hiddenCheckbox}
-                                onChange={handleInputChange}
+                                onChange={(e) => handleInputChange(e, formData, setFormData)}
                             />
                             <label htmlFor={`color-${color.slug}`} className={classes.colorLabel}>
                                 <p>{color.name}</p>
---
+++ src/utils/windowFormFunctions.js
@@ -0,0 +1,58 @@
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const navigate = useNavigate();
+const successNotification = () => toast.success('¡Producto cargado!');
+const alertNotification = () => toast.warn('Por favor, selecciona al menos un color.', { autoClose: 5000 });
+
+export const initializeFormData = (opening, style, type) => ({
+    product: 'ventana',
+    opening,
+    style,
+    type,
+    width: '',
+    height: '',
+    casement: '',
+    casement2: '',
+    quantity: '',
+    glassType: '',
+    colors: [],
+});
+
+// Función para manejar cambios de input (checkbox y otros)
+export const handleInputChange = (e, formData, setFormData) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+        setFormData(prevData => {
+            const updatedColors = checked
+                ? [...prevData.colors, value]
+                : prevData.colors.filter(color => color !== value);
+            return { ...prevData, colors: updatedColors };
+        });
+    } else {
+        setFormData(prevData => {
+            let newValue;
+            if (name === 'quantity') {
+                newValue = parseInt(value, 10) || '';
+            } else if (['width', 'height', 'casement', 'casement2'].includes(name)) {
+                newValue = parseFloat(value) || '';
+            } else {
+                newValue = value;
+            }
+            return { ...prevData, [name]: newValue };
+        });
+    }
+};
+
+// Función para enviar el formulario
+export const handleSubmit = (e, formData, addItem) => {
+    e.preventDefault();
+    if (formData.colors.length === 0) {
+        alertNotification();
+        return;
+    };
+    addItem(formData);
+    successNotification();
+    navigate('/');
+};
