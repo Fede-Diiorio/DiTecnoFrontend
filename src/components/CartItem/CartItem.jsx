2241fe8930a51f2c2f36ca509cdbfd63ae1c3456
Federico Di Iorio <federico_metallica@hotmail.com>
2024-09-23 19:28:38 -0300

    Se mejoró el componente de CartItem para que sea más simple y funcione de forma correcta

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,105 +1,78 @@
-import classes from './CartItem.module.scss';
-import { useCart } from '../../context/CartContext';
-import { useLocalStorage } from '../../context/LocalStorageContext';
-import { FaTrashCan } from 'react-icons/fa6';
 import { getWindowSpecification } from '../../utils/getWindows';
 import { getDoorModel } from '../../utils/getDoors';
 import { useEffect, useState } from 'react';
-import { toast } from "react-toastify";
+import WindowItem from './WindowItem';
+import DoorItem from './DoorItem';
+
+const Cart = ({ cartItems }) => {
+    return (
+        <div>
+            {cartItems.map((item, index) => (
+                <CartItem key={index} {...item} />
+            ))}
+        </div>
+    );
+};
 
 const CartItem = ({ product, type, quantity, id, style, colors, design, opening, casement, casement2, casement3, glassType, height, width }) => {
+    const [typeSpecification, setTypeSpecification] = useState(null);
 
-    const { removeItem } = useCart();
-    const { removeProductFromLocalStorage } = useLocalStorage();
-    const [typeSpecification, setTypeSpecification] = useState();
-    const successNotify = () => toast.success('Producto eliminado.');
-
-
-    if (product = 'ventana') {
-        useEffect(() => {
+    useEffect(() => {
+        if (product === 'ventana') {
             getWindowSpecification(opening, style, type).then(data => {
                 setTypeSpecification(data);
             });
-        }, [opening, style, type]);
-    } else {
-        useEffect(() => {
+        } else if (product === 'puerta') {
             getDoorModel(opening, style, type, design).then(data => {
                 setTypeSpecification(data);
             });
-        }, [opening, style, type, design]);
-    };
-
-
-    const deleteItem = () => {
-        successNotify();
-        removeProductFromLocalStorage(id);
-        removeItem(id);
-    }
+        }
+    }, [product, opening, style, type, design]);
 
     const colorsPayload = colors.join(' - ');
-    const casementPayload = casement ? casement : 'No requerido';
-    const casement2Payload = casement2 ? casement2 : 'No requerido';
-    const casement3Payload = casement3 ? casement3 : 'No requerido';
+    const casementPayload = casement || 'No requerido';
+    const casement2Payload = casement2 || 'No requerido';
+    const casement3Payload = casement3 || 'No requerido';
 
+    // Renderiza DoorItem si el producto es "puerta"
     if (product === 'puerta') {
         return (
-            <div className={classes.container}>
-                <h5 className={classes.title}>{product} abertura {opening} {style}</h5>
-                <div className={classes.product}>
-                    <img className={classes.image} src={typeSpecification.image} alt={`Imagen de ${product} ${style}`} />
-                    <div className={classes.information}>
-
-                        <div className={classes.dimensions}>
-                            <p className={classes.data}><strong>Alto (cm): </strong>{height}</p>
-                            <p className={classes.data}><strong>Ancho (cm): </strong>{width}</p>
-                            <p className={classes.data}><strong>Ancho de hoja (cm): </strong>{casementPayload}</p>
-                            <p className={classes.data}><strong>Alto de hoja (cm): </strong>{casement2Payload}</p>
-                            <p className={classes.data}><strong>Alto de hoja 2 (cm): </strong>{casement3Payload}</p>
-                        </div>
-
-                        <div className={classes.info}>
-                            <p className={classes.data}><strong>Colores: </strong>{colorsPayload}</p>
-                            <p className={classes.data}><strong>Cantidad: </strong>{quantity}</p>
-                        </div>
-
-                    </div>
-                    <button onClick={deleteItem}><FaTrashCan className={classes.icon} /></button>
-                </div>
-            </div>
+            <DoorItem
+                product={product}
+                opening={opening}
+                style={style}
+                image={typeSpecification?.image}
+                height={height}
+                width={width}
+                casementPayload={casementPayload}
+                casement2Payload={casement2Payload}
+                casement3Payload={casement3Payload}
+                colorsPayload={colorsPayload}
+                quantity={quantity}
+            />
         );
-    };
+    }
 
+    // Renderiza WindowItem si el producto es "ventana"
     if (product === 'ventana') {
-        if (!typeSpecification) {
-            return <p>Cargando especificaciones...</p>;
-        }
-
         return (
-            <div className={classes.container}>
-                <h5 className={classes.title}>{product} abertura {opening} {style}</h5>
-                <div className={classes.product}>
-                    <img className={classes.image} src={typeSpecification.image} alt={`Imagen de ${product} ${style}`} />
-                    <div className={classes.information}>
-
-                        <div className={classes.dimensions}>
-                            <p className={classes.data}><strong>Alto (cm): </strong>{height}</p>
-                            <p className={classes.data}><strong>Ancho (cm): </strong>{width}</p>
-                            <p className={classes.data}><strong>Ancho de hoja (cm): </strong>{casementPayload}</p>
-                            <p className={classes.data}><strong>Alto de hoja (cm): </strong>{casement2Payload}</p>
-                        </div>
-
-                        <div className={classes.info}>
-                            <p className={classes.data}><strong>Colores: </strong>{colorsPayload}</p>
-                            <p className={classes.data}><strong>Cantidad: </strong>{quantity}</p>
-                            <p className={classes.data}><strong>Vidrio DVH: </strong>{glassType}</p>
-                        </div>
-
-                    </div>
-                    <button onClick={deleteItem}><FaTrashCan className={classes.icon} /></button>
-                </div>
-            </div>
+            <WindowItem
+                product={product}
+                opening={opening}
+                style={style}
+                image={typeSpecification?.image}
+                height={height}
+                width={width}
+                casementPayload={casementPayload}
+                casement2Payload={casement2Payload}
+                colorsPayload={colorsPayload}
+                quantity={quantity}
+                glassType={glassType}
+            />
         );
-    };
+    }
+
+    return null; // Si no es ni "puerta" ni "ventana", no renderiza nada
 };
 
-export default CartItem;
+export default Cart;
---
+++ src/components/CartItem/WindowItem.jsx
@@ -0,0 +1,46 @@
+import classes from './CartItem.module.scss';
+import { useCart } from '../../context/CartContext';
+import { useLocalStorage } from '../../context/LocalStorageContext';
+import { toast } from 'react-toastify';
+import { FaTrashCan } from 'react-icons/fa6';
+
+const WindowItem = ({ product, opening, style, image, height, width, casementPayload, casement2Payload, colorsPayload, quantity, glassType }) => {
+
+    const { removeItem } = useCart();
+    const { removeProductFromLocalStorage } = useLocalStorage();
+    const successNotify = () => toast.success('Producto eliminado.');
+
+    const deleteItem = () => {
+        successNotify();
+        removeProductFromLocalStorage(id);
+        removeItem(id);
+    };
+
+    return (
+        <div className={classes.container}>
+            <h5 className={classes.title}>{product} abertura {opening} {style}</h5>
+            <div className={classes.product}>
+                <img className={classes.image} src={image} alt={`Imagen de ${product} ${style}`} />
+                <div className={classes.information}>
+
+                    <div className={classes.dimensions}>
+                        <p className={classes.data}><strong>Alto (cm): </strong>{height}</p>
+                        <p className={classes.data}><strong>Ancho (cm): </strong>{width}</p>
+                        <p className={classes.data}><strong>Ancho de hoja (cm): </strong>{casementPayload}</p>
+                        <p className={classes.data}><strong>Alto de hoja (cm): </strong>{casement2Payload}</p>
+                    </div>
+
+                    <div className={classes.info}>
+                        <p className={classes.data}><strong>Colores: </strong>{colorsPayload}</p>
+                        <p className={classes.data}><strong>Cantidad: </strong>{quantity}</p>
+                        <p className={classes.data}><strong>Vidrio DVH: </strong>{glassType}</p>
+                    </div>
+
+                </div>
+                <button onClick={deleteItem}><FaTrashCan className={classes.icon} /></button>
+            </div>
+        </div>
+    );
+};
+
+export default WindowItem;
